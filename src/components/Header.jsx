14dbed28dcf652760c6a71354c2d0365b5a91596
Dylan Manley <dylan.manley@gmail.com>
2020-03-25 14:41:23 -0700

    add styling to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,30 +6,43 @@ function Header(){
   const mainStyle = {
     backgroundColor: '#38171C',
     color: '#D2E8CC',
-    height: '42px',
+    height: 'calc(30px + 3vmin)',
     padding: '8px',
     paddingTop: '0px;',
     width: '100%',
     display: 'inline-flex',
   }
 
+  const headerTitleStyle = {
+    color: '#D2E8CC',
+    display: 'inline-flex',
+    height: 'calc(24px + 2vmin)',
+    fontSize: 'calc(12px + 2vmin)',
+    marginRight: '20vw',
+    border: '3px double #D2E8CC',
+    padding: '0.33vmin',
+  }
+
+
   const navLinksStyle = {
     color: '#D2E8CC',
     display: 'inline-flex',
-    marginLeft: '45vw',
+    marginTop: '1.2vmin',
   }
 
   const linkStyle = {
     marginLeft: '1.2vw',
     marginRight: '1.2vw',
+    color: '#D2E8CC',
+    fontSize: 'calc(6px + 2vmin)',
   }
 
   return (
     <div style={mainStyle}>
-      <h4>Header</h4>
+      <h4 style={headerTitleStyle}>Header</h4>
       <div style={navLinksStyle}>
-        <p style={linkStyle}><Link to="/">Home</Link></p>
-        <p style={linkStyle}><Link to="/signin">Sign In</Link></p>
+        <p><Link to="/" style={linkStyle}>Home</Link></p>
+        <p><Link to="/signin" style={linkStyle}>Sign In</Link></p>
       </div>
     </div>
   );
