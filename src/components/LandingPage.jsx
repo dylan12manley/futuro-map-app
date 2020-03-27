c87537c047dac4a903f86f268a41f0675ee4acd0
Dylan Manley <dylan.manley@gmail.com>
2020-03-26 20:26:46 -0700

    import images and display them on landingpage

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { }
+import A1 from './../assets/images/landing/A1.png'
+import E1 from './../assets/images/landing/E1.png'
+import F1 from './../assets/images/landing/F1.png'
+import G1 from './../assets/images/landing/G1.png'
+import I1 from './../assets/images/landing/I1.png'
+import M1 from './../assets/images/landing/M1.png'
+import N1 from './../assets/images/landing/N1.png'
+import O1 from './../assets/images/landing/O1.png'
+import R1 from './../assets/images/landing/R1.png'
+import T1 from './../assets/images/landing/T1.png'
+import U1 from './../assets/images/landing/u1.png'
+import Z1 from './../assets/images/landing/Z1.png'
+import mag1 from './../assets/images/landing/mag1.png'
+import FM1 from './../assets/images/landing/FM1.png'
+import futuro1 from './../assets/images/landing/futuro1.png'
+import futuro2 from './../assets/images/landing/futuro2.png'
+import Magazine1 from './../assets/images/landing/Magazine1.png'
+import Magazine2 from './../assets/images/landing/Magazine2.png'
+import Rings1 from './../assets/images/landing/Rings1.png'
 
 function LandingPage(){
   const styledLanding = {
@@ -11,6 +29,28 @@ function LandingPage(){
     fontFamily: 'Black Ops One',
     textAlign: 'center',
   }
+  const styledLetterImgs = {
+    height: '6vmin',
+    width: '6vmin',
+  }
+
+  const styledLetterImgs2 = {
+    height: 'calc(18vmin)',
+    width: 'calc(8px+13vmin)',
+    paddingBottom: '3vmin',
+    paddingLeft: '12vmin',
+  }
+
+  const styledMagImgs = {
+    height: '24vmin',
+    width: '33vmin',
+  }
+
+  const styledMagazineTitle = {
+    height: '24vmin',
+    width: '63vmin',
+  }
+
   return (
     <div style={styledLanding}>
       <h2 style={{
@@ -33,7 +73,46 @@ function LandingPage(){
       <div style={{ backgroundColor: '#485266',
                     color: '#D6684D',
                   }}>
-        <img src={}
+
+        <img src={F1} style={styledLetterImgs}></img>
+        <img src={U1} style={styledLetterImgs}></img>
+        <img src={T1} style={styledLetterImgs}></img>
+        <img src={U1} style={styledLetterImgs}></img>
+        <img src={R1} style={styledLetterImgs}></img>
+        <img src={O1} style={styledLetterImgs}></img>
+        <p>   </p>
+        <img src={M1} style={styledLetterImgs}></img>
+        <img src={A1} style={styledLetterImgs}></img>
+        <img src={G1} style={styledLetterImgs}></img>
+        <img src={A1} style={styledLetterImgs}></img>
+        <img src={Z1} style={styledLetterImgs}></img>
+        <img src={I1} style={styledLetterImgs}></img>
+        <img src={N1} style={styledLetterImgs}></img>
+        <img src={E1} style={styledLetterImgs}></img>
+      </div>
+
+      <div style={{ backgroundColor: '#9FC0E0',
+                    color: '#D6684D',
+                  }}>
+        <img src={FM1} style={styledMagImgs}></img>
+        <img src={F1} style={styledLetterImgs2}></img>
+        <img src={mag1} style={styledMagImgs}></img>
+      </div>
+
+      <div style={{ backgroundColor: '#485266',
+                    color: '#D6684D',
+                  }}>
+
+        <img src={futuro2} style={styledMagazineTitle}></img>
+        <img src={Magazine2} style={styledMagazineTitle}></img>
+      </div>
+
+      <div style={{ backgroundColor: '#9FC0E0',
+                    color: '#D6684D',
+                  }}>
+
+        <img src={futuro1} style={styledMagazineTitle}></img>
+        <img src={Magazine1} style={styledMagazineTitle}></img>
       </div>
     </div>
 
