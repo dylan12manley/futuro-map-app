c644155059a3f99de4e22bd09963fd1a4aa361b7
Dylan Manley <dylan.manley@gmail.com>
2020-03-26 19:13:42 -0700

    adjust styling to landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -7,29 +7,34 @@ function LandingPage(){
     color: '#8E545E',
     padding: '50px',
     height: '100vh',
-    fontFamily: 'Black Ops One'
+    fontFamily: 'Black Ops One',
+    textAlign: 'center',
   }
   return (
     <div style={styledLanding}>
       <h2 style={{
             color: '#D6684D',
             textDecoration: 'underline-overline',
-            border: '1.2px double #485266',
-         }}>LANDING<br>
-       </br>PAGE
+            border: '2vmin double #485266',
+            padding: '1vmin'
+         }}>FUTURO<br>
+       </br>MAGAZINE
       </h2>
       <div style={{
             backgroundColor: '#9FC0E0',
             border: '0.4px solid #4A5468',
             padding: '12px',
             }}>
-        <h3>GO
-          <Link to="/home" style={{ textDecoration: 'none',
-                           color: '#D6684D',
-              }}> HOME</Link>?
-        </h3>
+        <h3>GO<Link to="/home" style={{ textDecoration: 'none',
+                                        color: '#D6684D',
+                                      }}> HOME</Link>?</h3>
       </div >
+      <div style={{ backgroundColor: '#485266',
+                    color: '#D6684D',
+                  }}>
+      </div>
     </div>
+
   );
 }
 
