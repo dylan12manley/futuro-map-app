c9287aa4194a6d25fa0740d3625a3bfc019d17f8
Dylan Manley <dylan.manley@gmail.com>
2020-03-27 21:49:39 -0700

    add rows and column to home component

--- src/components/AdvertList.jsx
+++ src/components/AdvertList.jsx
@@ -1,48 +1,9 @@
 import React from "react";
-import dior from './../../images/dior.jpg'
-import keene from './../../images/keene.jpg'
-import arTecHouse from './../../images/artechouse.png'
-import taschen from './../../images/Taschen.png'
-import tein from './../../images/TEIN.jpg'
+import PropTypes from "prop-types";
 
-const advertList = [
-  {
-    title: 'ad number 1',
-    redirectUrl: 'https://www.keane-eyes.com/',
-    img: keene,
-    alt: 'Keene',
-    id: 1
-  },
-  {
-    title: 'ad number 2',
-    redirectUrl: 'https://www.dior.com/en_us',
-    img: taschen,
-    id: 2
-  },
-  {
-    title: 'ad number 3',
-    redirectUrl: 'https://www.dior.com/en_us',
-    img: dior,
-    alt: 'Dior',
-    id: 3
-  },
-  {
-    title: 'ad number 4',
-    redirectUrl: 'https://www.keane-eyes.com/',
-    img: arTecHouse,
-    alt: 'arTecHouse',
-    id: 4
-  }
-  {
-    title: 'ad number 5',
-    redirectUrl: 'https://www.store.teinclothing.com/',
-    img: tein,
-    alt: 'T.E.I.N.',
-    id: 5
-  }
-]
+import Advert from "./Advert";
 
-function AdvertList(){
+function AdvertList(props){
 
   return (
     <>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Header from "./Header";
 import FakeArticle from "./FakeArticle";
+import Advert from "./Advert";
 
 
 function Home(){
   return (
     <div >
-    <Header/>
-    <FakeArticle/>
+      <Header/>
+      <div className='row'>
+        <div className='column'>
+          <FakeArticle/>
+        </div>
+        <div className='column'>
+          <Advert/>
+        </div>
+      </div>
     </div>
 
   )}
