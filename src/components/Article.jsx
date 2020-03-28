a5be84210821fa4a4d43ee7de7a2591ddee97072
Dylan Manley <dylan.manley@gmail.com>
2020-03-27 20:52:53 -0700

    create article style page and add styling to article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import './../styles/article.css'
 
 function Article(props){
   return (
     <>
       <div>
-        <h4>{props.title}</h4>
-        <p><em>{props.headline}</em></p>
-        <p><em>{props.date}</em></p>
-        <h5>{props.author}</h5>
-        <img src={props.mainImg}></img>
-        <p>{props.body}</p>
+        <div className='articleTop'>
+          <h2 className='articleTitle'>{props.title}</h2>
+          <p>{props.author} <span className='date'>{props.date}</span></p>
+        </div>
+        <div className='articleBottom'>
+          <p>{props.headline}</p>
+          <img src={props.mainImg}></img>
+          <p>{props.body}</p>
+        </div>
       </div>
       <hr/>
     </>
