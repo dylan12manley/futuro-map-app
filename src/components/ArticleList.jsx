7ba336320bdd243a50280f5ed92c3f9372384516
Dylan Manley <dylan.manley@gmail.com>
2020-04-02 20:31:57 -0700

    make app display articles from firebase

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -3,26 +3,18 @@ import { useSelector } from 'react-redux';
 import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 import Article from "./Article";
 
-function ArticleList(props) {
+function ArticleList() {
 
-  const articles = useSelector(state => state.firestore.articles);
-  if (isEmpty) {
-    return( 
-      <div>
-        <p>No Articles Added Yet
-        </p>
-      </div>
-    )
-  }
-  else if (isLoaded(articles)) {
+  useFirestoreConnect([
+    { collection: 'articles' }
+  ]);
+
+  const articles = useSelector(state => state.firestore.ordered.articles);
+
+  if (isLoaded(articles)) {
     return (
       <>
-      <div style={{
-    marginTop: '55px',
-    padding: 'calc(8px + 1vmin)',
-    color: '#38171C',
-  }}>
-        {Object.values(props.articleList).map((article) => {
+        {Object.values(articles).map((article) => {
           return <Article
             title={article.title}
             headline={article.headline}
@@ -34,17 +26,22 @@ function ArticleList(props) {
             id={article.id}
             key={article.id}/>
         })}
-      </div>
       </>
     );
-  } else {
-    return (
-      <React.Fragment>
-        <h3>Loading...</h3>
-      </React.Fragment>
-    )
-  }
-
+  } else if (isEmpty) {
+      return( 
+        <div>
+          <p>No Articles Added Yet
+          </p>
+        </div>
+      )
+    } 
+  return (
+    <React.Fragment>
+      <h3>Loading...</h3>
+    </React.Fragment>
+  );
+  
 }
 
 
