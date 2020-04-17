28ca1d853dc77a790bc272eda06ca58394711180
Dylan Manley <dylan.manley@gmail.com>
2020-04-16 20:05:51 -0700

    create catagorie pages to hold the article lists

--- src/components/pages/FilmPage.jsx
+++ src/components/pages/FilmPage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "../shared/Header";
 import Footer from "../shared/Footer";
-import Art from "../pages/Art";
+import Film from "../pages/Film";
 import FakeAdvert from "../FakeAdvert";
 import Sidebar from "../shared/Sidebar";
 import { Row, Col } from 'antd';
 
 
 
-function ArtPage(){
+function FilmPage(){
 
   return (
     <div >
@@ -18,7 +18,7 @@ function ArtPage(){
         <Sidebar/>
         </Col>
         <Col  xs={24} sm={16} lg={17} xl={16} xxl={15}>
-          <Art/>
+          <Film/>
         </Col>
         <Col xs={24} sm={8} lg={7} xl={6} xxl={5}>
           <FakeAdvert/>
@@ -35,4 +35,4 @@ function ArtPage(){
 
   )}
 
-export default ArtPage;
+export default FilmPage;
