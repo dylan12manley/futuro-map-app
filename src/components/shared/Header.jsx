c134aca2571bfd8c385426093ad378ad17cbeff9
Dylan Manley <dylan.manley@gmail.com>
2020-05-13 16:48:10 -0700

    move the logo to a differnt row, on header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -33,6 +33,15 @@ function Header(){
     marginLeft: '5vmin',
   }
 
+  const navLinksStyle2 = {
+    color: '#F7F0ED',
+    marginTop: '1.5vmin',
+    marginRight: '5vmin',
+    position: 'fixed',
+    right: '0',
+    }
+ 
+
   const linkStyle = {
     marginLeft: '1.3vmin',
     marginRight: '1.3vmin',
@@ -57,18 +66,26 @@ function Header(){
         </div>
         </Col>
         <Col md={8} lg={4} xxl={2}>
-        <Link to="/" > 
+        {/* <Link to="/" > 
           <img src={futuro} style={futuroTitle} alt='Futuro'></img>
-        </Link>
+        </Link> */}
         </Col>
         <Col xxl={1} xxxl={1}></Col>
         <Col md={8} lg={10} xxl={1}>
-        <div style={navLinksStyle}>
+        <div style={navLinksStyle2}>
           <Link to="/people" style={linkStyle}>People</Link>
           <Link to="/art" style={linkStyle}>Adventures</Link>
           <Link to="/fashion" style={linkStyle}>Places</Link>
           <Link to="/film" style={linkStyle}>Store</Link>
         </div>
+        </Col>
+      </Row>
+      <Row style={{textAlign: 'center'}}>
+        <Col></Col>
+        <Col>
+          <Link to="/" > 
+            <img src={futuro} style={futuroTitle} alt='Futuro'></img>
+          </Link>
         </Col>
       </Row>
     </div>
