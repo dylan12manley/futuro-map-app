99c50283eb0bd56e619988962a8f4ee819f20452
Dylan Manley <dylan.manley@gmail.com>
2020-05-13 18:20:36 -0700

    make header links hide in a collapse when view window is small

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import futuro from '../../assets/images/BnRfuturo.png'
-import { Row, Col } from 'antd';
+import { Row, Col, Collapse } from 'antd';
+import { MenuUnfoldOutlined } from '@ant-design/icons';
+
+import '../../styles/Header.scss';
 
 function Header(){
+  
+  const { Panel } = Collapse;
 
   const mainStyle = {
     position: 'fixed',
@@ -16,15 +21,6 @@ function Header(){
     width: '100%',
     display: 'inline-flex',
     textAlign: 'center',
-  }
-
-  const futuroTitle = {
-    height: 'calc(40px + 5vmin)',
-    backgroundColor: 'black',
-    borderRadius: '10px',
-    padding: '3px',
-    marginTop: '40px', 
-    marginLeft: '2.5vmin',
   }
 
   const navLinksStyle = {
@@ -43,19 +39,25 @@ function Header(){
  
 
   const linkStyle = {
-    marginLeft: '1.3vmin',
-    marginRight: '1.3vmin',
+    marginLeft: '1.5vmin',
+    marginRight: '1.5vmin',
     color: '#F7F0ED',
     fontSize: 'calc(7px + 1.5vmin)',
     fontFamily: 'Bungee',
     fontWeight: '0'
   }
 
+  const linkStyle2 = {
+    color: 'black',
+    fontSize: 'calc(7px + 1vmin)',
+    fontFamily: 'Bungee',
+  }
+
   return (
     <>
     <div style={mainStyle}>
       <Row>
-        <Col md={8} lg={10} xxl={11}>
+        <Col xs={0} sm={0} md={0} lg={0} xl={10} xxl={11}>
         <div style={navLinksStyle}>
           <Link to="/art" style={linkStyle}>Art</Link>
           <Link to="/fashion" style={linkStyle}>Fashion</Link>
@@ -70,22 +72,45 @@ function Header(){
           <img src={futuro} style={futuroTitle} alt='Futuro'></img>
         </Link> */}
         </Col>
-        <Col xxl={1} xxxl={1}></Col>
-        <Col md={8} lg={10} xxl={1}>
-        <div style={navLinksStyle2}>
-          <Link to="/people" style={linkStyle}>People</Link>
-          <Link to="/art" style={linkStyle}>Adventures</Link>
-          <Link to="/fashion" style={linkStyle}>Places</Link>
-          <Link to="/film" style={linkStyle}>Store</Link>
-        </div>
+        <Col xl={0} xxl={0}></Col>
+        <Col xs={0} sm={0} md={0} lg={0} xl={10}xxl={11}>
+          <div style={navLinksStyle2}>
+            <Link to="/people" style={linkStyle}>People</Link>
+            <Link to="/art" style={linkStyle}>Adventures</Link>
+            <Link to="/fashion" style={linkStyle}>Places</Link>
+            <Link to="/film" style={linkStyle}>Store</Link>
+          </div>
         </Col>
       </Row>
       <Row style={{textAlign: 'center'}}>
-        <Col></Col>
-        <Col>
+        <Col xs={12} sm={12} md={12} lg={12} xl={24}>
           <Link to="/" > 
-            <img src={futuro} style={futuroTitle} alt='Futuro'></img>
+            <img src={futuro} className='logo' alt='Futuro'></img>
           </Link>
+        </Col>
+        <Col xs={8} sm={8} md={8} lg={8} xl={0} xxl={0}
+          className='collapse-col'>
+          <Collapse 
+            showArrow={false} 
+            bordered={false}
+            className='collapse'
+            expandIcon={({ isActive }) => <MenuUnfoldOutlined style={{fontSize: '24px', margin: '0, auto', backgroundColor: '#F7F0ED'}}rotate={isActive ? 90 : 360} />}
+            >
+            <Panel 
+              key="1" 
+              className="site-collapse-custom-panel">
+              <Link to="/people" style={linkStyle2}>People</Link><br/>
+              <Link to="/art" style={linkStyle2}>Adventures</Link><br/>
+              <Link to="/fashion" style={linkStyle2}>Places</Link><br/>
+              <Link to="/film" style={linkStyle2}>Store</Link><br/>
+              <Link to="/art" style={linkStyle2}>Art</Link><br/>
+              <Link to="/fashion" style={linkStyle2}>Fashion</Link><br/>
+              <Link to="/film" style={linkStyle2}>Film</Link><br/>
+              <Link to="/people" style={linkStyle2}>Humans</Link><br/>
+              <Link to="/people" style={linkStyle2}>Studios</Link><br/>
+              <Link to="/sound" style={linkStyle2}>Sound</Link><br/>
+            </Panel>
+          </Collapse>
         </Col>
       </Row>
     </div>
