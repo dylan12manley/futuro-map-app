0f039a6aaa04e295705ccafbfceff2e86a466903
Dylan Manley <dylan.manley@gmail.com>
2020-05-18 21:25:05 -0700

    add images to carousel

--- src/components/shared/MainCarousel.jsx
+++ src/components/shared/MainCarousel.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Carousel } from 'antd';
 import futuro from '../../assets/images/Futuro.jpg';
+import iOS2 from '../../assets/images/iOS2.jpg';
+import iOS3 from '../../assets/images/iOS3.jpg';
 
 const styledCarousel = {
   paddingTop: '75px',
@@ -16,12 +18,15 @@ const styledImage = {
 function MainCarousel(){
   return (
     <div style={styledCarousel}>
-      <Carousel autoplay>
+      <Carousel>
         <div>
             <img src={futuro} alt={futuro} style={styledImage}></img>
         </div>
         <div>
-          <img src={futuro} alt={futuro} style={styledImage}></img>
+          <img src={iOS2} alt={iOS2} style={styledImage}></img>
+        </div>
+        <div>
+          <img src={iOS3} alt={iOS3} style={styledImage}></img>
         </div>
     </Carousel>
     </div>
