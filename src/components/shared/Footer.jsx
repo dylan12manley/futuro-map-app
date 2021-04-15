ba1f4c314bbe1d7894ece6cd6d029c1b40dfbe65
dylan12manley <dylan.manley@gmail.com>
2021-04-14 21:28:09 -0700

    remove inline styling to css style sheets

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,37 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../../styles/Footer.scss'
 
 function Footer(){
 
-  const mainStyle = {
-    textAlign: 'right',
-    position: 'fixed',
-    zIndex: '1',
-    bottom: '0',
-    backgroundColor: 'black',
-    color: '#D2E8CC',
-    height: 'calc(26px + 2.5vmin)',
-    paddingTop: '0px',
-    width: '100%',
-    filter: 'grayscale(.75)',
-  }
-
-  const imagesStyle = {
-    backgroundColor: 'black',
-    display: 'inline-flex',
-    marginRight: '1.2vw',
-    height: '20px',
-  }
-
-
   return (
     <>
-    <div style={mainStyle}>
-        <div style={imagesStyle}>
-          <p>Contact us: <br></br>
-          <Link src='media@futuromagazine.com'>media@futuromagazine.com</Link></p>
-        </div>  
-    </div>
+      <div className='footer'>
+        <p style={{marginRight: '1.2vw'}}>Contact us: <br className='break'></br>
+        <Link src='media@futuromagazine.com'>media@futuromagazine.com</Link></p>    
+      </div>
     </>
   );
 }
